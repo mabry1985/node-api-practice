959c4729d854919de4d0264fcd903ccc8381fa39
Josh Mabry <mabry1985@gmail.com>
2019-12-18 09:50:15 -0800

    connect to mongo database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const app = express()
+const mongoose = require('mongoose');
+require("dotenv").config();
 
-const app = express();
+app.use(express.urlencoded({ extended:true}))
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-  res.send('We are home')
+const postsRoute = require('./routes/post')
+
+app.use('/posts', postsRoute)
+
+app.get('/', (req, res) => {
+    res.send('We are home!');
 })
+
+mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true}, () => console.log('connected to DB'))
 
 app.listen(3000);
 

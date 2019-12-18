f6f83c1c62b0b17dbf6d543c834cdf146ce7fe81
Josh Mabry <mabry1985@gmail.com>
2019-12-18 11:38:09 -0800

    add validation to register

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+
+  const { error } = registerValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+
+  const emailExist = await User.findOne({ email: req.body.email });
+  if(emailExist) return res.status(400).send('Email already exists');
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
